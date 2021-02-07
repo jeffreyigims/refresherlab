464517c9ad5582ebce8e432223f08260dd5792bd
Jeffrey Igims <jigims@andrew.cmu.edu>
2021-02-07 17:06:11 -0500

    Task model written

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,11 @@
 class Task < ApplicationRecord
     has_many :chores 
     has_many :children, through: :chores
+
+    scope :alphabetical, -> { order('name') }
+    scope :active,       -> { where(active: true) }
+
+    validates_presence_of :name
+    validates :points, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
+
 end
